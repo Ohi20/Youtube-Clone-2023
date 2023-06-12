5a1f104b3d3f209fdb77a2c7a1dea85329321129
Ohi20 <ashrafohi20@gmail.com>
2023-06-12 14:10:41 +0600

    Navbar-Updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
 import Search from '@mui/icons-material/Search';
 
 const SearchBar = () => {
-
-const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate();
 
   const onhandleSubmit = (e) => {
@@ -19,28 +18,32 @@ const [searchTerm, setSearchTerm] = useState('');
   };
 
   return (
-      <Paper
-      component='form'
+    <Paper
+      component="form"
       onSubmit={onhandleSubmit}
       sx={{
         borderRadius: 20,
-        border: '1px solid #e3e3e3',
+        border: '1px solid #000',
         pl: 2,
         boxShadow: 'none',
         mr: { sm: 5 },
       }}
     >
       <input
-        className='search-bar'
-        placeholder='Search...'
+        className="search-bar"
+        placeholder="Search..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+      <IconButton
+        type="submit"
+        sx={{ p: '10px', color: 'red' }}
+        aria-label="search"
+      >
         <Search />
       </IconButton>
     </Paper>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
